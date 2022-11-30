0d9ba5554e2b848fc6107ef98e00a39410a7235b
AnkitRaj5 <ankitraj6241@gmail.com>
2022-11-30 19:52:20 +0530

    no

--- app.js
+++ app.js
@@ -15,8 +15,42 @@ app.get('/api/products/:productId',(req,res)=>{
     console.log(req.params);
     const {productId} = req.params;
     const singleProduct = products.find((item)=>item.id === Number(productId))
+    if(!singleProduct){
+        return res.status(404).send("Product not found")
+    }
+    else
     res.json(singleProduct)
 })
+
+app.get('/api/products/:productId/reviews/:reviewId',(req,res)=>{
+    console.log(req.params);
+    res.send("hello there your review id is working")
+})
+app.get('/api/v1/query',(req,res)=>{
+    console.log(req.query);
+    const {search,limit} = req.query;
+    let sortedProduct = [...products];
+    // console.log(sortedProduct);
+    if(search){
+        sortedProduct = sortedProduct.filter((product)=>{
+            return product.name.startsWith(search)
+        })
+    }
+    if(limit){
+        sortedProduct = sortedProduct.slice(0,Number(limit));
+    }
+    if(sortedProduct.length<1){
+        // res.status(200).send('no product match to your search')
+        return res.status(200).json({sucess:true,data:[]})
+    }
+    return res.status(200).json(sortedProduct)
+    // res.send("you are in query section")
+})
+//input
+// http://localhost:5000/api/v1/query?search=a&limit=3
+//output
+// { name: 'ankit ', ' id': ' 5' }
+// { search: 'a', limit: '3' }
 
 // more optial way to do this
 // app.get('/api/products/1',(req,res)=>{
